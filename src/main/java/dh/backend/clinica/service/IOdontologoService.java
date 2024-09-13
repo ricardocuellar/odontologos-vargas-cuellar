2b7c1ac769fc27e5c26be53aedfef3d7188de1b8
LuisaVargas-9 <luisavr0997@gmail.com>
2024-09-13 20:08:00 -0300

    add implementations service

---
+++ src/main/java/dh/backend/clinica/service/IOdontologoService.java
@@ -0,0 +1,11 @@
+package dh.backend.clinica.service;
+
+import dh.backend.clinica.entity.Odontologo;
+import dh.backend.clinica.entity.Paciente;
+
+import java.util.Optional;
+
+public interface IOdontologoService {
+    Odontologo guardarOdontologo(Odontologo odontologo);
+    Optional<Odontologo> buscarPorId(Integer id);
+}
