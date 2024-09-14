5398926e48d7aa3f3cba108fde63d6e0dac786d9
LuisaVargas-9 <luisavr0997@gmail.com>
2024-09-14 02:28:26 -0300

    add loggers OdontologoService

--- src/main/java/dh/backend/clinica/service/impl/OdontologoService.java
+++ src/main/java/dh/backend/clinica/service/impl/OdontologoService.java
@@ -5,6 +5,9 @@ import dh.backend.clinica.entity.Paciente;
 import dh.backend.clinica.exception.ResourceNotFoundException;
 import dh.backend.clinica.repository.IOdontologoRepository;
 import dh.backend.clinica.service.IOdontologoService;
+import org.slf4j.ILoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,6 +15,7 @@ import java.util.Optional;
 
 @Service
 public class OdontologoService implements IOdontologoService {
+    public static final Logger logger = LoggerFactory.getLogger(OdontologoService.class);
 
     private IOdontologoRepository odontologoRepository;
 
@@ -21,11 +25,13 @@ public class OdontologoService implements IOdontologoService {
 
     @Override
     public Odontologo guardarOdontologo(Odontologo odontologo) {
+        logger.info("odontólogo guardado: " + odontologo);
         return odontologoRepository.save(odontologo);
     }
 
     @Override
     public void modificarOdontologo(Odontologo odontologo) {
+        logger.info("odontólogo modificado: " + odontologo);
         odontologoRepository.save(odontologo);
     }
 
@@ -33,6 +39,7 @@ public class OdontologoService implements IOdontologoService {
     public void eliminarOdontologo(Integer id) {
         Optional<Odontologo> odontologoEncontrado = buscarPorId(id);
         if(odontologoEncontrado.isPresent()){
+            logger.info("id del odontólogo eliminado: " + id);
             odontologoRepository.deleteById(id);
         }else{
             throw new ResourceNotFoundException("Paciente no encontrado");
@@ -47,6 +54,7 @@ public class OdontologoService implements IOdontologoService {
 
     @Override
     public Optional<Odontologo> buscarPorId(Integer id) {
+        logger.info("id del odontólogo encontrado: " + id);
         return odontologoRepository.findById(id);
     }
 }
